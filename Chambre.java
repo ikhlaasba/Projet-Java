0fdc5fbb1c7b88d1cfc7b72d85a2a137dd5750ed
Arijbk04 <142218221+Arijbk04@users.noreply.github.com>
2024-12-05 09:22:58 +0100

    class chambre

---
+++ Chambre.java
@@ -0,0 +1,31 @@
+public class Chambre{
+    private int num;
+    private String type;
+    private String statut;
+    public Chambre( int num,String type,String statut){
+        this.num=num;
+        this.type=type;
+        this.statut=statut;
+    }
+     public void setNum(int num){
+        this.num=num;
+    }
+    public void setType(String type){
+        this.type=type;
+    }
+    public void setStatut(String statut){
+        this.statut=statut;
+    }
+     public int getNum(){
+        return this.num;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public String getStatut(){
+        returnt this.statut;
+    }
+    public String toString(){
+        return "Num: "+this.num+"Type: "+this.type+"Statut: "+this.statut;
+    }
+}
