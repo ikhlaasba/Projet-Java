fb477f31f5d1c621a5dc3a1968b2606ee44b4c6d
ikhlaasba <bahiaikhlas@gmail.com>
2024-12-16 19:35:25 +0100

    SeeMe

---
+++ SeeMe.java
@@ -0,0 +1,8 @@
+/*changes made by ikhlas :
+-zedt 2 classes : DAL.java & DBConnection.java
+-badlt f service.java 5ater ma5doum blghalt
+-zedt attribut esmou id_resident fl classe resident.java 5atr nst7a9ouh fl bdd
+-badlt type mta3 l'attribut dossier_medical radetou string fi Resident 5atr manajmch n5dm liste fl bdd 
+-3malt compile lel files kol adhakaaleh tzadou .class files 
+-zedt SeeMe File 
+*/
