91489797f9becfca2179569e5f4644db8df64bfa
sourourboudhiba11 <sourour.bouthiba@isimg.tn>
2024-12-17 16:19:15 +0100

    controller3

---
+++ manager/src/Controller3.java
@@ -0,0 +1,43 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class controller3 {
+
+    @FXML
+    private Button b1;
+
+    @FXML
+    private Button b2;
+
+    @FXML
+    private Button b3;
+
+    @FXML
+    private Button b4;
+
+    @FXML
+    private Label lab1;
+
+    @FXML
+    void click1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click4(ActionEvent event) {
+
+    }
+
+}
